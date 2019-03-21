3127f03a4a04e6cd7045b2bd6add59d859742734
zhu <husteiczhu@163.com>
2019-03-21 11:37:14 +0800

    3/20project

--- jianshu/jianshu.js
+++ jianshu/jianshu.js
@@ -1,38 +1,47 @@
+var slideshow = document.querySelector(".slideshow");
 var wrap = document.querySelector(".wrap");
 var next = document.getElementById("arrowright");
 var prev = document.getElementById("arrowleft");
 
-
-//var next = document.querySelector(".arrow_right");
-//var prev = document.querySelector(".arrow_left");
 next.onclick = function () {
-    alert("be here");
     next_pic();
 }
 prev.onclick = function () {
     prev_pic();
 }
-/*next.addEventListener("click",fn,false);
-function fn(){
-    alert("be here");
-}*/
+
 function next_pic () {
-    alert("success");
     var newLeft;
-    if(wrap.style.left === "-2400px"){
-        newLeft = -600;
+    if(wrap.style.left === "-1200px"){
+        newLeft = 0;
     }else{
         newLeft = parseInt(wrap.style.left)-600;
     }
     wrap.style.left = newLeft + "px";
-    alert("been here");
 }
 function prev_pic () {
     var newLeft;
     if(wrap.style.left === "0px"){
-        newLeft = -2400;
+        newLeft = -1200;
     }else{
         newLeft = parseInt(wrap.style.left)+600;
     }
     wrap.style.left = newLeft + "px";
+
 }
+
+var timer;
+function autoPlay () {
+      setInterval(function () {
+      next_pic();
+  },3000);
+}
+autoPlay();
+
+/*slideshow.onmouseover = function(){
+  clearInterval(timer);
+}
+slideshow.onmouseout = function(){
+  autoPlay();
+}*/
+
